6dfc1d7ebd2e5b3bdb92639fec56224f0ee34e68
GabrieleAggio <g.aggio@dstech.it>
2025-05-13 22:43:41 +0200

    esercizio2 component test

--- src/app/esercizi/esercizio2/esercizio2.component.spec.ts
+++ src/app/esercizi/esercizio2/esercizio2.component.spec.ts
@@ -1,23 +1,43 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { configureTestModule } from '../esercizi.spec.utils';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateFakeLoader } from '@ngx-translate/core';
-
 import { Esercizio2Component } from './esercizio2.component';
+import { EserciziService } from 'src/app/services/esercizi.service';
+import { of } from 'rxjs';
 
 describe('Esercizio2Component', () => {
   let component: Esercizio2Component;
   let fixture: ComponentFixture<Esercizio2Component>;
 
-   beforeEach(async () => {
+  let service: EserciziService;
+
+  beforeEach(async () => {
     await configureTestModule(Esercizio2Component);
     fixture = TestBed.createComponent(Esercizio2Component);
+    service = TestBed.inject(EserciziService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Dovrebbe sottoscriversi a observer component1', () => {
+    const observable = [1, 2, 3];
+
+    //Mock del creaObservableEsericizio1
+    spyOn(service, 'creaObservableEsercizio1').and.returnValue(
+      of(...observable)
+    );
+
+    const logSpy = spyOn(console, 'log');
+
+    service.creaObserverEsercizio2();
+
+    expect(service.creaObservableEsercizio1).toHaveBeenCalled();
+    expect(logSpy).toHaveBeenCalledWith('valore ricevuto: ', 1);
+    expect(logSpy).toHaveBeenCalledWith('valore ricevuto: ', 2);
+    expect(logSpy).toHaveBeenCalledWith('valore ricevuto: ', 3);
+    expect(logSpy).toHaveBeenCalledWith('Observable completato');
   });
 });
